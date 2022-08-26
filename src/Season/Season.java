8cdb15ddec6106d2827c89a804466d747350060e
Alpimax <alperrozkann@gmail.com>
2022-08-26 19:43:54 -0400

    Season class created

---
+++ src/Season/Season.java
@@ -0,0 +1,24 @@
+package Season;
+
+public class Season {
+    String name;
+    double highest;
+    double lowest ;
+
+    public Season(String name, double highest, double lowest) {
+        this.name = name;
+        this.highest = highest;
+        this.lowest = lowest;
+    }
+
+    public void activity(){
+
+        return ;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+
+    }
+}
