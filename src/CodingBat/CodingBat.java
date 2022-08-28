6ad8b0b3fa69cd31254b774d7438870607267352
Alpimax <alperrozkann@gmail.com>
2022-08-28 01:31:18 -0400

    class created

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1,23 +1,31 @@
 package CodingBat;
 
+import java.util.Arrays;
+
 public class CodingBat {
     public static void main(String[] args) {
+    int[] nums ={2,4,11,3};
 
-        System.out.println(method("zzzopzop"));
+
+        System.out.println(Arrays.toString(twoSum(nums,6)));
     }
 
-    public static String method(String str) {
-            String sr = "";
-            for (int i = 0 ; i < str.length()-2; i ++ )
-
-                if (str.charAt(i)=='z'&& str.charAt(i+2)=='p'){
-                    sr += str.charAt(i)+""+str.charAt(i+2);
-                    i+=2;
-                }else
-                    sr+= str.charAt(i);
-
-
-            return sr;
-
+    public static int[] twoSum(int[] nums, int target) {
+        int count = 0;
+        String str = String.valueOf(count);
+        int [] number = new int[2];
+        for(int each : nums)  {
+            for(int i = 0 ; i<nums.length ;i++){
+                if (count != i) {
+                    if ((each + nums[i]) == target) {
+                        number[0] = count;
+                        number[1] = i;
+                        return number;
+                    }
+                }
+            }
+            count++;
         }
+        return number;
+    }
 }
