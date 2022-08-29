7141d38cf461d5f28400c8d29d525fe33f7af9ce
Alpimax <alperrozkann@gmail.com>
2022-08-29 10:43:20 -0400

    Season class created

--- src/Day41_exceptions/Season.java
+++ src/Day41_exceptions/Season.java
@@ -1,4 +1,13 @@
 package Day41_exceptions;
 
-public class Season {
+public class Summer extends Season{
+
+    public Summer(double highestTemp, double lowestTemp) {
+        super("Summer", highestTemp, lowestTemp);
+    }
+
+    @Override
+    public void activity() {
+        System.out.println("Drink Lemonade");
+    }
 }
