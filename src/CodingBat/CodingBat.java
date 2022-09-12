97df96cd315d7cd5a0c7f3feb26e21d9a97f8202
Alpimax <alperrozkann@gmail.com>
2022-09-12 10:41:07 -0400

    Class created

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -6,18 +6,5 @@ public class CodingBat {
     public static void main(String[] args) {
 
 
-        System.out.println(method("12xy34", "1"));
-    }
-
-
-        Arrays.sort(nums);
-        int sum = nums[nums.length-1]-nums[0];
-
-return  str;
-        }
-    public static int[] method(String a, String b) {
-    int [] nums = {1,5,3,6,73};
-
-    return nums;
     }
 }
